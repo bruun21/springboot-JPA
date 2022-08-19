ccf1152650bc2b16524f3dde5775e032b9296f92
bruun21 <brunocrfraga@gmail.com>
2022-08-19 09:47:54 -0300

    Project created

---
+++ src/main/java/com/example/SPBJPA/SpbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SPBJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SPBJPA/SpbJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SPBJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
