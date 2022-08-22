4cc784402b347dd9e6702936e57468279144f29e
bruun21 <brunocrfraga@gmail.com>
2022-08-22 08:55:45 -0300

    Category

---
+++ src/main/java/com/example/SPBJPA/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.SPBJPA.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.SPBJPA.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
